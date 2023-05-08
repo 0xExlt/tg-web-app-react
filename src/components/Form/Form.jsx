2a63c26d53268867db1f3f6d490bb62e593a5bd8
0xEx <sam.sam200@mail.ru>
2023-05-08 19:01:18 +0300

    init

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,5 +1,4 @@
-import React from "react";
-import { useEffect, useState } from "react"
+import React, { useEffect, useState } from "react"
 import { useTelegram } from "../../hooks/useTelegram";
 import './Form.css';
 
