b6ea49e46c6677172b085745630f0b3f946bdb28
0xEx <sam.sam200@mail.ru>
2023-05-08 22:19:40 +0300

    init

--- src/components/ProductItem/ProductItem.jsx
+++ src/components/ProductItem/ProductItem.jsx
@@ -10,7 +10,9 @@ const ProductItem = ({product, className, onAdd}) => {
 
     return (
         <div className={'product ' + className}>
-            <div className={'img'}>{product.imageUrl}</div>
+            <div className={'img'}>
+            <img src="img1.png" alt="" />
+            </div>
             <div className={'title'}>{product.title}</div>
             <div className={'description'}>{product.description}</div>
             <div className={'price'}>
